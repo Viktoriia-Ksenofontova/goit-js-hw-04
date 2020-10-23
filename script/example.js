f3f7c9e456a010d909e3f75ff61d59d643267809
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2020-10-23 09:38:29 +0300

    примерчики

--- script/example.js
+++ script/example.js
@@ -17,28 +17,69 @@
 
 
 // 2
-const filter = function(array, test) {
-  const filteredElements = [];
+// const filter = function(array, test) {
+//   const filteredElements = [];
 
-  for (const element of array) {
-    const passed = test(element);
+//   for (const element of array) {
+//     const passed = test(element);
 
-    if (passed) {
-      filteredElements.push(element);
-    }
+//     if (passed) {
+//       filteredElements.push(element);
+//     }
+//   }
+
+//   return filteredElements;
+// };
+
+// const fruits = [
+//   { name: 'apples', quantity: 200, isFresh: true },
+//   { name: 'grapes', quantity: 150, isFresh: false },
+//   { name: 'bananas', quantity: 100, isFresh: true },
+// ];
+
+// const freshFruits = filter(fruits, fruit => fruit.isFresh);
+// console.log(freshFruits); // массив с объектами apples и bananas
+
+// const fruitsWithQuantity = filter(fruits, fruit => fruit.quantity >= 120);
+// console.log(fruitsWithQuantity); // массив с объектами apples и grapes
+
+// 3
+// Setup
+var collection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
   }
-
-  return filteredElements;
 };
 
-const fruits = [
-  { name: 'apples', quantity: 200, isFresh: true },
-  { name: 'grapes', quantity: 150, isFresh: false },
-  { name: 'bananas', quantity: 100, isFresh: true },
-];
+// Only change code below this line
+function updateRecords(object, id, prop, value) {
+  if (value === "") {
+    delete object[id][prop];
+  }
+  else if (prop === "tracks") {
+    object[id][prop] = object[id][prop] || [];
+    object[id][prop].push(value);
+  }
+  else {
+    object[id][prop] = value;
+  }
+     
+  
+  return object;
+}
 
-const freshFruits = filter(fruits, fruit => fruit.isFresh);
-console.log(freshFruits); // массив с объектами apples и bananas
-
-const fruitsWithQuantity = filter(fruits, fruit => fruit.quantity >= 120);
-console.log(fruitsWithQuantity); // массив с объектами apples и grapes
+console.log (updateRecords(collection, 5439, 'artist', 'ABBA'));
--- script/task-4-5.js
+++ script/task-4-5.js
@@ -42,3 +42,6 @@ console.log(invokeRemove);
 console.log(arrayRemove);
 // ['Knife', 'Medkit']
 
+
+
+  
